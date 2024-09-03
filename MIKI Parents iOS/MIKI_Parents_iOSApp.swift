cbc858e0f53ea558b5db3b282849a86dd2395b02
danielgabler <gabler-eventmanagement@gmx.de>
2024-09-03 16:17:17 +0200

    Initial Commit

---
+++ MIKI Parents iOS/MIKI_Parents_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MIKI_Parents_iOSApp.swift
+//  MIKI Parents iOS
+//
+//  Created by Daniel Gabler on 03.09.24.
+//
+
+import SwiftUI
+
+@main
+struct MIKI_Parents_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MIKI Parents iOS/README.md
@@ -0,0 +1,13 @@
+# MIKI Parents iOS
+
+## Beschreibung
+Eine kurze Beschreibung des Projekts.
+
+## Installation
+Schritte zur Installation des Projekts.
+
+## Nutzung
+Beispiele, wie man das Projekt verwendet.
+
+## Lizenz
+Informationen zur Lizenzierung.
