e32461f1e1478c693b6054f92b429628fc3c293f
danielgabler <gabler-eventmanagement@gmx.de>
2024-09-18 15:36:32 +0200

    Push ready

---
+++ MIKI Parents iOS/Homework/FullScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  FullScreenView.swift
+//  MIKI Parents iOS
+//
+//  Created by Daniel Gabler on 18.09.24.
+//
+
+import SwiftUI
+
+struct FullScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FullScreenView()
+}
