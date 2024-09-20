70997009c3cb085cccc5cc0f87dfa9e92bbcfe26
danielgabler <gabler-eventmanagement@gmx.de>
2024-09-20 13:06:13 +0200

    Update Fullscreen View

--- MIKI Parents iOS/Homework/FullScreenView.swift
+++ MIKI Parents iOS/Homework/FullScreenView.swift
@@ -8,11 +8,46 @@
 import SwiftUI
 
 struct FullScreenView: View {
+    var file: FileItem
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            if file.type == "image" {
+                if let url = URL(string: file.imageUrl) {
+                    AsyncImage(url: url) { phase in
+                        switch phase {
+                        case .success(let image):
+                            image
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        case .failure:
+                            Image(systemName: "photo")
+                                .resizable()
+                                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        case .empty:
+                            ProgressView()
+                        @unknown default:
+                            ProgressView()
+                        }
+                    }
+                }
+                
+            } else {
+                Text("Dokument anzeigen")
+                    .font(.largeTitle)
+                    .padding()
+               
+            }
+        }
+        .background(Color.black.edgesIgnoringSafeArea(.all))
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            ToolbarItem(placement: .cancellationAction) {
+                Button("Schließen") {
+                    
+                }
+            }
+        }
     }
-}
-
-#Preview {
-    FullScreenView()
 }
